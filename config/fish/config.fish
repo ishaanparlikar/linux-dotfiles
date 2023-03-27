09a433b8c99a6925f04e9cd567ddce20e03749e3
Ishan Parlikar <iparlika@emich.edu>
2023-03-27 19:18:16 +0530

    Fish Config

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias lvim  "~/.local/bin/lvim"
+
+function nvm
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+set PATH ~/.nvm/versions/node/*/bin $PATH
