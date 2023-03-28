6431b4a1b77fe4521e5cc73a3ff8d16b6b72d28d
Ishan Parlikar <iparlika@emich.edu>
2023-03-29 03:00:39 +0530

    added shortcut for vertical split

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -20,6 +20,8 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+lvim.keys.normal_mode["<leader>sv"] = ":vsplit<CR>"
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
