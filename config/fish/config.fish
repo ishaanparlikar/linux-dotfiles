d6aff1f7bf97ef347935b4cae7e7dfea2ba88987
ishaanparlikar <iparlika@emich.edu>
2023-06-17 02:26:58 +0530

    added nvchad config, neofetch config, updated fish and alacritty config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,5 +9,14 @@ end
 set PATH ~/.nvm/versions/node/*/bin $PATH
 
 
-xmodmap -e "remove Lock = Caps_Lock" -e "keysym Caps_Lock = Escape"
+# xmodmap -e "remove Lock = Caps_Lock" -e "keysym Caps_Lock = Escape"
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/hazelnut/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+# Neofetch
+neofetch
