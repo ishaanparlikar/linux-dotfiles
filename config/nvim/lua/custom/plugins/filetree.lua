43ff7a4701d454caf98faaa9759188733586ff31
Ishan Parlikar <iparlika@emich.edu>
2024-04-02 10:10:17 +0530

    added plugins

--- config/nvim/lua/custom/plugins/filetree.lua
+++ config/nvim/lua/custom/plugins/filetree.lua
@@ -1,14 +1,15 @@
 -- File: lua/custom/plugins/filetree.lua
 
 return {
-    "nvim-neo-tree/neo-tree.nvim",
+    "nvim-tree/nvim-tree.lua",
     version = "*",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+    dependencies = { 
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
     },
     config = function ()
-      require('neo-tree').setup {}
+      require('nvim-tree').setup {
+      }
     end,
   }
