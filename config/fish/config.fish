954b58f797ed1a7664bae671fa6276d6b5149050
ishaanparlikar <iparlika@emich.edu>
2024-04-02 09:59:02 +0530

    fish updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,29 +1,14 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-alias lvim  "~/.local/bin/lvim"
-set -gx EDITOR nvim
-set fish_greeting
-function nvm
-    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
-set PATH ~/.nvm/versions/node/*/bin $PATH
-
-
-# xmodmap -e "remove Lock = Caps_Lock" -e "keysym Caps_Lock = Escape"
 starship init fish | source
 
-# pnpm
-set -gx PNPM_HOME "/home/hazelnut/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-# Neofetch
-fastfetch
-
-thefuck --alias | source
+set -gx PATH "$HOME/.cargo/bin" $PATH;
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/ishan/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
