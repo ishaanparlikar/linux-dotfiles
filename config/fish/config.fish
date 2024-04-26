95e2873a18bba539b00b63ad8529bec8286732dc
Ishan Parlikar <iparlika@emich.edu>
2024-04-26 18:13:50 +0530

    added zoxide

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,8 +7,7 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 # pnpm
-set -gx PNPM_HOME "/home/ishan/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
+set -gx PNPM_HOME "/home/hazelnut/.local/share/nvm/v21.7.2/bin/pnpm"
+
+#zoxide 
+zoxide init fish | source
