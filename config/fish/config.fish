ee7b3e2775f911c360bc678effb202ce9a932ef1
Ishan Parlikar <iparlika@emich.edu>
2024-04-27 15:03:28 +0530

    fish update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,10 +4,11 @@ set -gx PATH "$HOME/.cargo/bin" $PATH;
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
+set --export PATH $BUN_INSTALL/bin $PATH;
 # pnpm
 set -gx PNPM_HOME "/home/hazelnut/.local/share/nvm/v21.7.2/bin/pnpm"
+
+set --export PATH "$HOME/.local/bin/" $PATH;
 
 #zoxide 
 zoxide init fish | source
